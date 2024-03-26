cf858cfa69826fc3906a6a63f962b0079b5a0a3b
azolfagharir <azolfagharir@gmail.com>
2024-03-26 22:38:19 +0330

    About us is responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <>
-      <div className="w-72 ml-4 bg-white shadow-xl rounded-lg text-gray-900  top-0">
+      <div className=" top-20 absolute w-72 ml-4 md:w-80 lg:w-96 lg:ml-16 md:ml-8   2xl:ml-60 2xl:absolute bg-white shadow-xl rounded-lg text-gray-900   ">
         <div className="rounded-t-lg h-32 overflow-hidden">
           <img
             className="object-cover object-top w-full"
@@ -9,9 +9,9 @@ const About = () => {
             alt="Mountain"
           />
         </div>
-        <div className="mx-auto md:mx-0 w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
+        <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
           <img
-            className="object-cover object-center h-32"
+            className="object-cover object-center h-32 "
             src="/Babak.jpg"
             alt="Woman looking front"
           />
@@ -22,7 +22,7 @@ const About = () => {
         </div>
         <div>
           <p className="text-center" dir="rtl">
-            من 17 ساله هستم و عاشق c# و mvc
+            عاشق برنامه نویسی بکند به خصوص c# هستم
           </p>
         </div>
         <div className="p-4 border-t mx-8 mt-2">
@@ -34,7 +34,7 @@ const About = () => {
         </div>
       </div>
 
-      <div className=" w-72 mr-4 bg-white shadow-xl rounded-lg text-gray-900 top-16 absolute end-0">
+      <div className="top-20 absolute  w-72 mr-4 md:w-80 md:mr-8 lg:w-96 lg:mr-16 xl:mr-32 bg-white shadow-xl rounded-lg text-gray-900 end-0 2xl:mr-60">
         <div className="rounded-t-lg h-32 overflow-hidden">
           <img
             className="object-cover object-top w-full"
