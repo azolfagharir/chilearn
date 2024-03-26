e7b38bb716f37003730952bc77681ba0ec8d0d5c
azolfagharir <azolfagharir@gmail.com>
2024-03-27 01:47:01 +0330

    tutorial page is going to be completed

--- src/pages/TutorialYoung.jsx
+++ src/pages/TutorialYoung.jsx
@@ -1,24 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import Header from "../component/Header";
 import GameScreen from "../component/GameScreen";
 
-function Tutorial() {
+const TheTutorial = [
+  {
+    Title: "سلام",
+    VideoAdress: "/salam.mp4",
+    discription:
+      "سلام یک کلمه خیلی خوب و دوست داشتنیه! وقتی به کسی سلام می‌کنی، احساس خوبی بهش منتقل می‌شه. همیشه باید با مهربونی و لبخند به کسی سلام بگی.  ",
+  },
+  {},
+];
+
+function TutorialYoung() {
+  const [isSoundPlaying, setIsSoundPlaying] = useState(false);
+  const toggleSound = () => {
+    setIsSoundPlaying(!isSoundPlaying);
+  };
   return (
-    <>
-      <GameScreen />
-      <div>
-        <h1>درباره ما</h1>
-        <p>خوش آمدید به صفحه درباره ما!</p>
-        <p>
-          ما یک تیم از توسعه دهندگان هستیم که به ارائه راهکارهای نرم‌افزاری با
-          کیفیت برای مشتریان علاقه‌مند هستیم. ما به دنبال بهبود تجربه کاربری و
-          ارائه راهکارهای مبتنی بر فناوری هستیم که به افراد و کسب‌وکارها کمک
-          کنند به دستاوردهای بزرگ‌تری دست یابند.
-        </p>
-        <p>اگر سوالی دارید، با ما تماس بگیرید!</p>
+    <div className="container mx-auto py-8 xl:mx-8 2xl:mx-24">
+      <br className="hidden md:block" />
+      <br className="hidden md:block" />
+      <br className="hidden md:block" />
+      <br />
+      <br />
+      <h1 className="text-3xl font-bold mb-4 text-center">
+        {TheTutorial[0].Title}
+      </h1>
+      <div className="relative w-full h-96">
+        <video className="w-full h-full object-cover" controls>
+          <source src={TheTutorial[0].VideoAdress} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="absolute top-0 right-0 m-4 bg-white p-2 rounded-full">
+          {/* You can replace the icon with your speaker icon */}
+        </div>
       </div>
-    </>
+      <p dir="rtl" className="mt-4 text-lg text-gray-800 text-center">
+        {TheTutorial[0].discription}
+      </p>
+      <br />
+      <div className="grid justify-center">
+        <button onClick={toggleSound}>
+          <img className="w-16" src="/volume.png" alt="" />
+        </button>
+        {isSoundPlaying && (
+          <audio autoPlay>
+            <source src="/salam.mp3" type="audio/mp3" />
+            Your browser does not support the audio element.
+          </audio>
+        )}
+      </div>
+    </div>
   );
 }
-
-export default Tutorial;
+export default TutorialYoung;
