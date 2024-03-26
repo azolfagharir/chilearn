0088e25b3e610272bcdbceb33b43fef415a45ad8
azolfagharir <azolfagharir@gmail.com>
2024-03-27 00:17:17 +0330

    sign up is responsive

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -157,191 +157,124 @@ function SignUp() {
   ];
   return (
     <>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <br />
-      <div>
-        <section className="bg-gray-50 dark:bg-gray-900 text-center top-28 absolute mx-28  md:top-28 md:absolute xl: ">
-          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a
-              href="#"
-              className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-            >
-              <img className="w-8 h-8 mr-2" src="/my_logo.png" alt="logo" />
-              چیلرن
-            </a>
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  ساخت اکانت
-                </h1>
-                <form className="space-y-4 md:space-y-6" action="#">
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      ایمیل شما
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="name@company.com"
-                      required=""
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      رمز
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      required=""
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      تکرار رمز
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      required=""
-                    />
-                  </div>
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="terms"
-                        aria-describedby="terms"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required=""
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="terms"
-                        className="font-light text-gray-500 dark:text-gray-300"
-                      >
-                        قوانین را{" "}
-                        <a
-                          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                          href="#"
-                        >
-                          قبول می کنم
-                        </a>
-                      </label>
-                    </div>
-                  </div>
-                  {my_questions.map((question, index) => (
-                    <Questions
-                      key={index}
-                      question={question.question}
-                      options={question.options}
-                      selectedOption={selectedOption[question.id] || ""}
-                      handleChange={(event) => handleChange(event, question.id)}
-                      id={nanoid()}
-                    />
-                  ))}
-                  <button
-                    type="submit"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      <section className="bg-gray-50 mx-12  md:mx-20 lg:mx-36 xl:mx-48 2xl:mx-64 dark:bg-gray-900 text-center py-8 w-full">
+        <div className="container mx-auto">
+          <Link
+            to={PathConstants.Home}
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img className="w-8 h-8 mr-2" src="/my_logo.png" alt="logo" />
+            چیلرن
+          </Link>
+          <div className="w-full max-w-lg mx-auto bg-white rounded-lg shadow-md dark:border dark:border-gray-700">
+            <div className="p-6 space-y-4">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
+                ساخت اکانت
+              </h1>
+              <form className="space-y-4" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    <Link to="/TutorialYoung"></Link>
-                    ساخت اکانت
-                  </button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    اکانت دارم{" "}
-                    <Link
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      to={PathConstants.Login}
+                    ایمیل شما
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    رمز
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    تکرار رمز
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="terms"
+                      className="font-light text-gray-500 dark:text-gray-300"
                     >
-                      (ورود)
-                    </Link>
-                  </p>
-                </form>
-              </div>
+                      قوانین را{" "}
+                      <a
+                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                        href="#"
+                      >
+                        قبول می کنم
+                      </a>
+                    </label>
+                  </div>
+                </div>
+                {my_questions.map((question, index) => (
+                  <Questions
+                    key={index}
+                    question={question.question}
+                    options={question.options}
+                    selectedOption={selectedOption[question.id] || ""}
+                    handleChange={(event) => handleChange(event, question.id)}
+                    id={nanoid()}
+                  />
+                ))}
+                <button
+                  type="submit"
+                  className="bg-gray-900 text-white rounded-lg px-4 py-2 mx-auto block w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg hover:bg-gray-800 transition duration-300"
+                >
+                  ساخت اکانت
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  اکانت دارم{" "}
+                  <Link
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    to={PathConstants.Login}
+                  >
+                    (ورود)
+                  </Link>
+                </p>
+              </form>
             </div>
           </div>
-        </section>
-      </div>
-      <br />
-      <br />
-      <p></p>
-      <br />
-      <p></p>
-      <br />
-      <p></p>
-      <br />
-      <p></p>
+        </div>
+      </section>
     </>
   );
 }
